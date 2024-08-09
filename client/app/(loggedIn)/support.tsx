5fbc2ebcdb40b3dd24ac3e86a6e58e301c85afbe
AdamTLeuty <adamthomasleuty@gmail.com>
2024-08-09 15:19:27 +0100

    Adds accordion element for support page FAQs

--- client/app/(loggedIn)/support.tsx
+++ client/app/(loggedIn)/support.tsx
@@ -3,13 +3,15 @@ import React, { useState, useEffect } from "react";
 
 import EditScreenInfo from "@/components/EditScreenInfo";
 import LoginArea from "@/components/LoginArea";
-import { Text, View } from "@/components/Themed";
+import { Text, View, ScrollView } from "@/components/Themed";
 
 import { useUserContext } from "@/components/userContext";
 import { router } from "expo-router";
 import { getToken } from "@/services/tokenStorage";
 
 import LogoLinkCard from "@/components/logoLinkCard";
+
+import Accordion from "@/components/accordion";
 
 //import { useRoute } from "@react-navigation/native";
 
@@ -28,16 +30,44 @@ export default function Support() {
   }, [isLoggedIn]);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Customer Support</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <Text style={styles.content}>We want to make sure that you have the best experience from start to finish.</Text>
-      <View style={styles.supportCards}>
-        <LogoLinkCard text="Call Us" iconName="phone" link="tel:${03308226507}" linkType="phone" />
-        <LogoLinkCard text="Email Us" iconName="mail" link="mailto:info@smilecorrectclub.co.uk" linkType="mail" />
-        <LogoLinkCard text="Live Chat" iconName="support" link="/liveChat" linkType="screen" />
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.title}>Customer Support</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <Text style={styles.content}>We want to make sure that you have the best experience from start to finish.</Text>
+        <View style={styles.supportCards}>
+          <LogoLinkCard text="Call Us" iconName="phone" link="tel:${03308226507}" linkType="phone" />
+          <LogoLinkCard text="Email Us" iconName="mail" link="mailto:info@smilecorrectclub.co.uk" linkType="mail" />
+          <LogoLinkCard text="Live Chat" iconName="support" link="/liveChat" linkType="screen" />
+        </View>
+
+        <Accordion
+          buttonText={"How does Smile Correct Club work?"}
+          hiddenText={
+            "After your free e-consultation with one of our experts, we’ll send your at-home impression kit straight to your door. Use our easy-to-follow video guide to provide your impressions. If you’re eligible, we’ll create your moulds, manufacture your aligners, and send them to you within 2-3 weeks of receiving your impressions. They’ll be posted straight to your door, and your smile journey can begin right away."
+          }
+        />
+
+        <Accordion
+          buttonText={"How much do clear aligners cost?"}
+          hiddenText={"Our single-arch aligners are £1,295 or £26.96 per month. Our dual-arch clear aligners are £1,595 or £33.21 per month."}
+        />
+
+        <Accordion
+          buttonText={"How long do I have to wear clear aligners?"}
+          hiddenText={"For the best results, your clear aligners need to be worn for 20-22 hours every day. Ideally, you should only remove them to eat, drink and brush your teeth."}
+        />
+
+        <Accordion
+          buttonText={"How long will the treatment take?"}
+          hiddenText={
+            "Our average aligner treatment plan is between 4-7 months. Some treatment plans may be shorter and some may be longer - it’s completely dependent on the complexity of your case."
+          }
+        />
+
+        <Accordion buttonText={"Are you Smile Direct Club?"} hiddenText={"No! We are Smile Correct Club, and are not affiliated with Smile Direct Club in any way."} />
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
---
+++ client/components/accordion.tsx
@@ -0,0 +1,144 @@
+import Animated, { useAnimatedStyle, useDerivedValue, useSharedValue, withTiming, SharedValue } from "react-native-reanimated";
+
+import { Text } from "./Themed";
+
+import { View } from "./Themed";
+
+import { Pressable, StyleSheet, SafeAreaView, Button } from "react-native";
+
+interface Props {
+  buttonText: String;
+  hiddenText: String;
+}
+
+function AccordionItem({ isExpanded, children, viewKey, style, duration = 500 }) {
+  const height = useSharedValue(0);
+
+  const derivedHeight = useDerivedValue(() =>
+    withTiming(height.value * Number(isExpanded.value), {
+      duration,
+    }),
+  );
+  const bodyStyle = useAnimatedStyle(() => ({
+    height: derivedHeight.value,
+  }));
+
+  return (
+    <Animated.View key={`accordionItem_${viewKey}`} style={[styles.animatedView, bodyStyle, style]}>
+      <View
+        onLayout={(e) => {
+          height.value = e.nativeEvent.layout.height;
+        }}
+        style={styles.wrapper}
+      >
+        {children}
+      </View>
+    </Animated.View>
+  );
+}
+
+type ItemProps = {
+  children: string;
+};
+
+function Item({ children }: ItemProps) {
+  return (
+    <Text style={styles.hidden_text} fontWeight="400">
+      {children}
+    </Text>
+  );
+}
+
+type ParentProps = {
+  open: SharedValue<boolean>;
+  children: string;
+};
+
+function Parent({ open, children }: ParentProps) {
+  return (
+    <View style={styles.parent}>
+      <AccordionItem style={styles.none} isExpanded={open} viewKey="Accordion">
+        <Item>{children}</Item>
+      </AccordionItem>
+    </View>
+  );
+}
+
+const Accordion: React.FC<Props> = ({ buttonText, hiddenText }) => {
+  const open = useSharedValue(false);
+  const onPress = () => {
+    open.value = !open.value;
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Pressable onPress={onPress}>
+        <Text style={styles.accordionText} fontWeight="600">
+          {buttonText}
+        </Text>
+      </Pressable>
+      <Parent open={open}>{hiddenText}</Parent>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  none: {},
+  container: {
+    borderColor: "#00000000",
+    borderTopColor: "#F7F6F8",
+    borderBottomColor: "#F7F6F8",
+    borderWidth: 1,
+    borderStyle: "solid",
+    paddingVertical: 13,
+    paddingLeft: 17,
+    paddingRight: 45,
+    width: "100%",
+  },
+  accordionText: {
+    fontSize: 16,
+    color: "#000",
+  },
+  hidden_text: {
+    fontSize: 16,
+    color: "#BDBDBD",
+    width: "100%",
+    paddingTop: 10,
+  },
+  buttonContainer: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  parent: {
+    width: "100%",
+  },
+  wrapper: {
+    width: "100%",
+    position: "absolute",
+    display: "flex",
+    alignItems: "center",
+  },
+  animatedView: {
+    width: "100%",
+    overflow: "hidden",
+  },
+  box: {
+    height: 120,
+    width: 120,
+    color: "#f8f9ff",
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "green",
+  },
+});
+
+export default Accordion;

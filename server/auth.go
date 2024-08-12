f11abd06ecb46f475e5097f68b2078d19e7c2490
AdamTLeuty <adamthomasleuty@gmail.com>
2024-08-12 09:19:18 +0100

    Adds extra hour to auth token to account for timezone (Server)

--- server/auth.go
+++ server/auth.go
@@ -13,7 +13,7 @@ func createToken(email string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
 		jwt.MapClaims{
 			"email": email,
-			"exp":   time.Now().Add(time.Hour * 1).Unix(),
+			"exp":   time.Now().Add(time.Hour * 2).Unix(),
 		})
 
 	tokenString, err := token.SignedString(secretKey)

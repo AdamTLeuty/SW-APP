477fc2857a29aa8b3e949fd2ac0f6abfc1ec0dc3
AdamTLeuty <adamthomasleuty@gmail.com>
2024-08-20 13:36:18 +0100

    Changes splash screen, and adds check to keep splash screen until fonts loaded (Client)

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -3,11 +3,14 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native
 import { useFonts } from "expo-font";
 import { router, Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
+import { useEffect, useState, useCallback } from "react";
 import "react-native-reanimated";
 import { UserProvider, useUserContext } from "@/components/userContext";
 import { ImageProvider, useCurrentImageContext } from "@/components/currentImageContext";
 import { useColorScheme } from "@/components/useColorScheme";
+import * as Font from "expo-font";
+import { View } from "@/components/Themed";
+import { InteractionManager } from "react-native";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -25,40 +28,54 @@ SplashScreen.preventAutoHideAsync();
 import { Status } from "@/components/userContext";
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    Poppins_Black: require("@/assets/fonts/Poppins-Black.ttf"),
-    Poppins_BlackItalic: require("@/assets/fonts/Poppins-BlackItalic.ttf"),
-    Poppins_Bold: require("@/assets/fonts/Poppins-Bold.ttf"),
-    Poppins_BoldItalic: require("@/assets/fonts/Poppins-BoldItalic.ttf"),
-    Poppins_ExtraBold: require("@/assets/fonts/Poppins-ExtraBold.ttf"),
-    Poppins_ExtraBoldItalic: require("@/assets/fonts/Poppins-ExtraBoldItalic.ttf"),
-    Poppins_ExtraLight: require("@/assets/fonts/Poppins-ExtraLight.ttf"),
-    Poppins_ExtraLightItalic: require("@/assets/fonts/Poppins-ExtraLightItalic.ttf"),
-    Poppins_Italic: require("@/assets/fonts/Poppins-Italic.ttf"),
-    Poppins_Light: require("@/assets/fonts/Poppins-Light.ttf"),
-    Poppins_LightItalic: require("@/assets/fonts/Poppins-LightItalic.ttf"),
-    Poppins_Medium: require("@/assets/fonts/Poppins-Medium.ttf"),
-    Poppins_MediumItalic: require("@/assets/fonts/Poppins-MediumItalic.ttf"),
-    Poppins_Regular: require("@/assets/fonts/Poppins-Regular.ttf"),
-    Poppins_SemiBold: require("@/assets/fonts/Poppins-SemiBold.ttf"),
-    Poppins_SemiBoldItalic: require("@/assets/fonts/Poppins-SemiBoldItalic.ttf"),
-    Poppins_Thin: require("@/assets/fonts/Poppins-Thin.ttf"),
-    Poppins_ThinItalic: require("@/assets/fonts/Poppins-ThinItalic.ttf"),
-    ...FontAwesome.font,
-  });
-
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+  const [appIsReady, setAppIsReady] = useState(false);
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          Poppins_Black: require("@/assets/fonts/Poppins-Black.ttf"),
+          Poppins_BlackItalic: require("@/assets/fonts/Poppins-BlackItalic.ttf"),
+          Poppins_Bold: require("@/assets/fonts/Poppins-Bold.ttf"),
+          Poppins_BoldItalic: require("@/assets/fonts/Poppins-BoldItalic.ttf"),
+          Poppins_ExtraBold: require("@/assets/fonts/Poppins-ExtraBold.ttf"),
+          Poppins_ExtraBoldItalic: require("@/assets/fonts/Poppins-ExtraBoldItalic.ttf"),
+          Poppins_ExtraLight: require("@/assets/fonts/Poppins-ExtraLight.ttf"),
+          Poppins_ExtraLightItalic: require("@/assets/fonts/Poppins-ExtraLightItalic.ttf"),
+          Poppins_Italic: require("@/assets/fonts/Poppins-Italic.ttf"),
+          Poppins_Light: require("@/assets/fonts/Poppins-Light.ttf"),
+          Poppins_LightItalic: require("@/assets/fonts/Poppins-LightItalic.ttf"),
+          Poppins_Medium: require("@/assets/fonts/Poppins-Medium.ttf"),
+          Poppins_MediumItalic: require("@/assets/fonts/Poppins-MediumItalic.ttf"),
+          Poppins_Regular: require("@/assets/fonts/Poppins-Regular.ttf"),
+          Poppins_SemiBold: require("@/assets/fonts/Poppins-SemiBold.ttf"),
+          Poppins_SemiBoldItalic: require("@/assets/fonts/Poppins-SemiBoldItalic.ttf"),
+          Poppins_Thin: require("@/assets/fonts/Poppins-Thin.ttf"),
+          Poppins_ThinItalic: require("@/assets/fonts/Poppins-ThinItalic.ttf"),
+          ...FontAwesome.font,
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
     }
-  }, [loaded]);
 
-  if (!loaded) {
+    prepare();
+  }, []);
+
+  useEffect(() => {
+    if (appIsReady) {
+      // Ensure that all interactions (like layout) are complete before hiding the splash screen
+      InteractionManager.runAfterInteractions(async () => {
+        await SplashScreen.hideAsync();
+      });
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
     return null;
   }
 
